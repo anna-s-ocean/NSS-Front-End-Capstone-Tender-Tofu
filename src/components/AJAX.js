a9ca55d14e0a297875ecf5c0e48c276fb55cbc49
anna-moore <ammoore434@gmail.com>
2021-01-29 15:50:27 -0600

    file creation

--- src/components/AJAX.js
+++
@@ -1,8 +0,0 @@
-$.ajax({
-    url      : 'https://api.yelp.com/v3/businesses/search?term=vegan&latitude=36.174465&longitude=-86.767960',
-    dataType : 'jsonp',
-    data     : {term : 'restaurant', lat : xxx, long : xxx}, // callback is not necessary
-    success  : function(data) {
-        // data is a normal response shown on yelp's API page
-    }
-});
---
+++ src/components/auth/Login.js
@@ -0,0 +1,66 @@
+mport React, { useRef } from "react"
+import { Link } from "react-router-dom";
+import { useHistory } from "react-router-dom"
+import "./Login.css"
+
+//how does this need to be refactored to Kennel
+
+export const Login = props => {
+    const email = useRef()
+    const password = useRef()
+    const existDialog = useRef()
+    const history = useHistory()
+
+    const existingUserCheck = () => {
+        return fetch(`http://localhost:8088/user?email=${email.current.value}`)
+            .then(res => res.json())
+            .then(user => user.length ? user[0] : false)
+    }
+
+    const handleLogin = (e) => {
+        e.preventDefault()
+
+        existingUserCheck()
+            .then(exists => {
+                if (exists) {
+                    localStorage.setItem("user_tender_tofu", exists.id)
+                    history.push("/")
+                } else {
+                    existDialog.current.showModal()
+                }
+            })
+    }
+
+    return (
+        <main className="container--login">
+            <dialog className="dialog dialog--auth" ref={existDialog}>
+                <div>User does not exist</div>
+                <button className="button--close" onClick={e => existDialog.current.close()}>Close</button>
+            </dialog>
+
+            <section>
+                <form className="form--login" onSubmit={handleLogin}>
+                    <h1>Tender Tofu</h1>
+                    <h2>Please sign in</h2>
+                    <fieldset>
+                        <label htmlFor="inputEmail"> Email address </label>
+                        <input ref={email} type="email"
+                            id="email"
+                            className="form-control"
+                            placeholder="Email address"
+                            required autoFocus />
+                    </fieldset>
+                    <fieldset>
+                        <button type="submit">
+                            Sign in
+                        </button>
+                    </fieldset>
+                </form>
+            </section>
+            <section className="link--register">
+                <Link to="/register">Not a member yet?</Link>
+            </section>
+        </main>
+    )
+}
+
