839cd4b49b7061463be076ab2796e1914fcadb16
anna-moore <ammoore434@gmail.com>
2021-02-23 06:50:15 -0600

    change landing page

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -1,6 +1,3 @@
-
-
-
 import React from "react"
 import { Link } from "react-router-dom"
 import { Logout } from "../auth/Logout"
@@ -16,7 +13,6 @@ export const NavBar = (props) => {
             {/* <li className="navbar__item">
                 <Link className="navbar__link" to="/users">Users</Link>
             </li> */}
-            {/* <div className="navbar__links"> */}
                 <li className="navbar__item">
                     <Link className="navbar__link" to="/restaurantOuting">New Restaurant Outings</Link>
                 </li>
@@ -24,7 +20,7 @@ export const NavBar = (props) => {
                     <Link className="navbar__link" to="/plannedOutings">Planned Restaurant Outings</Link>
                 </li>
                 <li className="navbar__item">
-                    <Link className="navbar__link" to="/halfwayFilledOutEateryFormNowFriendSelects">Friend's turn to swipe</Link>
+                    <Link className="navbar__link" to="/halfwayFilledOutEateryFormNowFriendSelects">Friend's Turn to Swipe</Link>
                 </li>
                 <li className="navbar__item logout">
                     <Logout />
